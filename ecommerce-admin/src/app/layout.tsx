e68f348789fd2717ffc6ea1f6ec84d36c50f8db1
Prathamesh Chavan <prathamesh.chavan@noovosoft.com>
2024-05-07 18:05:38 +0530

    fix: remove turborepo, adds 2 apps with tailwindcss

---
+++ ecommerce-admin/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Dope shop admin",
+  description: "Created using nextjs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
